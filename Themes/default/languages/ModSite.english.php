230c7a41f878e2eec4000ccff66367906dbe35dc
Suki <suki@missallsunday.com>
2013-01-01 17:47:31 -0600

    text strings
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/languages/ModSite.english.php
+++ Themes/default/languages/ModSite.english.php
@@ -2,70 +2,7 @@
 
 global $txt;
 
-$txt['MS_title'] = 'Mod Site';
-$txt['MS_mods'] = 'Mods';
-$txt['MS_themes'] = 'Themes';
-$txt['MS_translations'] = 'Traducciones';
-$txt['MS_enviar'] = 'Enviar archivo';
-$txt['MS_title_enviar'] = 'Formulario Modsite ohara';
-$txt['MS_save_title'] = 'Gracias por enviar tu archivo.';
-$txt['MS_save'] = 'Gracias por enviar tu archivo, Recuerda que tu archivo no aparecerá automaticamente, primero será revisado por el equipo para revisar su funcionalidad, esto puede llevar algo de tiempo así que no desesperes si tu mod no aparece, si piensas que nos hemos tardado demasiado en revisar tu mod, puedesn enviarnos un mensaje privado.';
-$txt['archivo_novalido'] = 'Tipo de archivo no válido, solo se permiten este tipo de extensiones:<p />
-<br />-"pdf"
-<br />-"txt"
-<br />-"zip"
-<br />-"gif"
-<br />-"png"
-<br />-"jpeg"
-<br />-"jpg"
-<br />-"php"';
-$txt['MS_tipo_mod'] = 'Modificación';
-$txt['MS_tipo_theme'] = 'Theme';
-$txt['MS_tipo_tra'] = 'Traducción';
-$txt['nombre_mod'] = 'Nombre del mod/theme o traducción:';
-$txt['necesita_tinydes'] = 'Se necesita una descripción corta.';
-$txt['necesitas_subirarchivo'] = 'Necesitas subir un archivo.';
-$txt['necesitas_tipo'] = 'Nesecitas especificar el tipo de archivo que vas a subir.';
-$txt['necesitas_version'] = 'Nesecitas especificar la version de SMF compatible con el archivo que vas a subir.';
-$txt['necesitas_nombre'] = 'Se necesita especificar un nombre para el archivo que vas a subir.';
-$txt['necesitas_descripcion'] = 'Debes de escribir una pequeña descripción para tu archivo, recuerda, puedes usar código BBC,  código HTML NO está permitido.';
-$txt['MS_version'] = 'Version de SMF:';
-$txt['MS_tipo'] = 'Tipo de Archivo:';
-$txt['MS_tipo_des'] = 'Selecciona el tipo de archivo que quieres subir, mod, theme o traducción.';
-$txt['MS_version_des'] = 'Selecciona la versión de SMF compatible con tu mod/theme o traducción.';
-$txt['MS_descripcion'] = 'Descripción:';
-$txt['MS_descripcion_des'] = 'Una descripción larga y detallada de tu archivo, puedes usar código bbc y smilies,  código html NO está permitido ni soportado. Aquí puedes incluir imágenes de tu mod o theme, instrucciones para tu traducción, etc.';
-$txt['MS_archivosubir'] = 'Archivo a subir:';
-$txt['MS_tinydes'] = 'Descripción corta:';
-$txt['MS_tinydes_des'] = 'Máximo 45 caracteres, escribe una descripción corta de tu mod/theme o traducción.';
-$txt['necesitas_tinydes'] = 'Necesitas agregar una descripción corta de tu archivo.';
-$txt['permissiongroup_modsiteper'] = 'Permisos para el mod site';
-$txt['permissiongroup_simple_modsiteper'] = 'Permisos para el mod site';
-$txt['permissionname_modsiteperview'] = 'Ver el mod site';
-$txt['permissionname_modsiteperedit'] = 'Editar el mod site';
-$txt['permissionname_modsiteperadmin'] = 'Administrar el mod site';
-$txt['cannot_modsiteperview'] = 'Lo siento, tu no puedes ver el Mod Site de Ohara';
-$txt['cannot_modsiteperedit'] = 'Lo siento, tu no puedes enviar archivos a nuestro mod site.';
-$txt['cannot_modsiteperadmin'] = 'Lo siento, tu no puedes acceder a esta sección';
-$txt['MS__mods_title'] = 'Listado de Mods';
-$txt['MS_mods_aprobar'] = 'Aprobar mods, themes y demás...';
-$txt['MS_usuario'] = 'Hecho por';
-$txt['MS_version'] = 'Compatible con';
-$txt['MS_descargas'] = 'Descargas';
-$txt['MS_tiny_des'] = 'Descripción';
-$txt['MS_descarga'] = 'Descarga';
-$txt['MS_vacio'] = 'Actualmente no hay mods listados en nuestra base de datos.';
-$txt['MS_vacio_tra'] = 'Actualmente no hay traducciones listadas en nuestra base de datos.';
-$txt['MS_vacio_themes'] = 'Actualmente no hay themes listados en nuestra base de datos.';
-$txt['MS_title_tra'] = 'Listado de traducciones';
-$txt['MS_title_save'] = 'Gracias por tu envio';
-$txt['MS_title_themes'] = 'Listado de themes';
-$txt['MS_mod_noaprobado'] = 'Este archivo aún no ha sido aprobado para su descarga, solo puede ser visto por los admins y por el usuario que envió el archivo.';
-$txt['nota'] = 'Nota, este topic permanecerá cerrado hasta que sea aprobado por un miembro del equipo, la probación puede tardar algo de tiempo dependiendo de la complejidad de el mod/theme/traducción, si crees que ha pasado mucho tiempo, puedes mandar un mensaje privado a la cuenta/bot Ohara Team  y un miembro del team te contestará a la brevedad.
-Si llegara a haber errores en tu archivo, se te contactará por mensaje privado acerca de esos errores apra que puedas corregirlos antes de que sea aprobado tu archivo.
-
-Este mensaje desaparecerá y el topic se abrirá cuando tu archivo sea aprobado.';
-$txt['MS_mods_editar'] = 'Plantilla para editar.';
-$txt['borrar_archivo'] = 'Marca esta casilla para borrar el archivo antiguo.';
-$txt['borrar_archivo_des'] = 'Desmarca esta casilla si quieres subir un nuevo archivo, al marcarla tendrás que subir tu nuevo archivo en la sección de abajo.';
+$txt['ModSite_title'] = 'Mod Site';
+$txt['ModSite_mods'] = 'Mods';
+$txt['ModSite_themes'] = 'Themes';
 ?>
