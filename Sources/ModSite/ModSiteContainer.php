3c674c654e2fa74d9d2eef4594e4885e48387e40
Suki <suki@missallsunday.com>
2013-01-01 12:06:13 -0600

    Alpha 1
    Signed-off-by: Suki <suki@missallsunday.com>

--- Sources/ModSite/ModSiteContainer.php
+++ Sources/ModSite/ModSiteContainer.php
@@ -25,20 +25,5 @@
  *
  */
 
-	if (file_exists(dirname(__FILE__) . '/SSI.php') && !defined('SMF'))
-		require_once(dirname(__FILE__) . '/SSI.php');
-
-	elseif (!defined('SMF'))
-		exit('<b>Error:</b> Cannot install - please verify you put this in the same place as SMF\'s index.php.');
-
-	$hooks = array(
-		'integrate_pre_include' => '$sourcedir/ModSite::$name/ModSite.php',
-		'integrate_admin_areas' => 'ModSiteHooks::admin',
-		'integrate_menu_buttons' => 'ModSiteHooks::menu',
-		'integrate_actions' => 'ModSiteHooks::action',
-	);
-
-	$call = 'add_integration_function';
-
-	foreach ($hooks as $hook => $function)
-		$call($hook, $function);
+if (!defined('SMF'))
+	die('Hacking attempt...');
