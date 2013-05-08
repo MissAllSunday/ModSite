2833ba99a595c27e5df73874fb50f4e6db3d98ab
Suki <suki@missallsunday.com>
2013-05-07 20:12:21 -0500

    re-write the template
    Signed-off-by: Suki <suki@missallsunday.com>

--- Themes/default/ModSite.template.php
+++ Themes/default/ModSite.template.php
@@ -34,7 +34,7 @@ function template_modSite_main()
 	/* Sidebar */
 	modsite_sideBar();
 
-	/* Show a nice message if no FAQs are avaliable */
+	/* Show a nice message if no mods are avaliable */
 	echo '
 	<div class="floatright nopadding" style="width:80%;">
 		<div class="cat_bar">
@@ -71,225 +71,38 @@ function template_modSite_add()
 {
 	global $context, $settings, $options, $scripturl, $modSettings, $txt;
 
+	modsite_header();
+
+	/* Sidebar */
+	modsite_sideBar();
+
 	/* The main div */
 	echo '
-	<div class="floatright nopadding" style="width:80%">';
+	<div class="floatright nopadding" style="width:80%;">';
 
+	/* A nice form for adding a new cat */
+	if ($context['faq']['object']->permissions('add') == true)
 		echo '
-		<form action="', $scripturl, '?action=modsite;sa=add2;', (!empty($context['modSite']['edit']) ? 'fid='.  $context['modSite']['edit']['id'] .';edit' : ''),'" method="post" target="_self" id="postmodify" class="flow_hidden" onsubmit="submitonce(this);smc_saveEntities(\'postmodify\', [\'name\', \'description\']);" >
-			<div class="cat_bar">
-				<h3 class="catbg">
-					',(!empty($context['modSite']['edit']) ?  $txt['modSite_edit_editing'] .' - '. $context['modSite']['edit']['name'] : $txt['modSite_edit_creating']),'
-				</h3>
-			</div>
-			<span class="clear upperframe">
-				<span></span>
-			</span>
-			<div class="roundframe rfix">
-				<div class="innerframe">
-					<dl id="post_header">';
-
-			/* Name */
-			echo '
+		<span class="clear upperframe">
+			<span></span>
+		</span>
+		<div class="roundframe rfix">
+			<div class="innerframe">
+				<form action="', $scripturl, '?action='. modsite::$name .';sa=add;step=2" method="post" target="_self">
+					<dl id="post_header">
 						<dt>
 							<span id="caption_subject">', $txt['modSite_edit_name'] ,'</span>
 						</dt>
 						<dd>
-							<input type="text" name="name" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['name'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* File */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_file'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="file" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['file'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* Demo link */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_demo'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="demo" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['demo'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* Mod version */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_version'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="version" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['version'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* ID topic for support */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_topic'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="topic" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['topic'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* SMF version */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_smf'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="smf" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['smf_version'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* SMF download */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_smfd'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="smfd" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['smf_download'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* Github repo name */
-			echo '
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_github'] ,'</span>
-						</dt>
-						<dd>
-							<input type="text" name="github" size="55" tabindex="1" maxlength="255" value="', (!empty($context['modSite']['edit']) ? $context['modSite']['edit']['github'] : '') ,'" class="input_text" />
-						</dd>';
-
-			/* Category select field */
-			echo'
-						<dt>
-							<span id="caption_subject">', $txt['modSite_edit_category'] ,'</span>
-						</dt>
-						<dd>';
-
-			/* Show the category select field */
-			if (!empty($context['modSite']['cats']))
-			{
-				echo '
-							<select name="id_category">';
-
-				foreach($context['modSite']['cats'] as $cats)
-					echo '
-								<option value="', $cats['id'] ,'" ', (isset($context['modSite']['edit']['cat']['id']) && $cats['id'] == $context['modSite']['edit']['cat']['id'] ? 'selected="selected"' : '') ,'>', $cats['name'] ,'</option>';
-
-				echo '
-							</select>';
-			}
-
-			else
-				echo '
-							<div class="faqmod_warning">
-								', $txt['modSite_edit_category_no'] ,'
-							</div>';
-
-			echo'
+							<input type="text" name="title" size="55" tabindex="1" maxlength="255" value="" class="input_text" /> <input type="submit" name="send" class="sbtn" value="', $txt['modSite_edit_add'] ,'" />
 						</dd>
-					</dl>';
-
-			if ($context['show_bbc'])
-				echo '
-						<div id="bbcBox_message"></div>';
-
-			if (!empty($context['smileys']['postform']) || !empty($context['smileys']['popup']))
-				echo '
-						<div id="smileyBox_message"></div>';
-
-			echo template_control_richedit($context['post_box_name'], 'smileyBox_message', 'bbcBox_message');
-
-			echo '
-						<div id="confirm_buttons">
-							<input type="hidden" id="', $context['session_var'], '" name="', $context['session_var'], '" value="', $context['session_id'], '" />
-							<input type="submit" name="send" class="sbtn" value="',(!empty($context['modSite']['edit']) || !empty($_REQUEST['previewEdit']) ? $txt['modSite_edit_edit'] : $txt['modSite_edit_add']),'" />
-							<input type="submit" name="preview" class="sbtn" value="', $txt['preview'], '" />
-						</div>
-					</div>
+					</dl>
+				</form>
 			</div>
-			<span class="lowerframe">
-				<span></span>
-			</span><br />
-		</form>';
-
-	echo '
-	</div>
-	<div class="clear"></div>';
-}
-
-function template_modSite_manageCat()
-{
-	global $context, $txt, $scripturl;
-
-	/* The main div */
-	echo '
-	<div class="floatright nopadding" style="width:80%">';
-
-	echo '
-		<div class="cat_bar">
-			<h3 class="catbg">', $txt['modSite_edit_cat_title'] ,'</h3>
 		</div>
-		<div class="windowbg description">
-			', $txt['modSite_edit_cat_desc'] ,'
-		</div>';
-
-		echo '
-			<table class="table_grid" cellspacing="0" width="100%">
-				<thead>
-					<tr class="catbg">
-						<th scope="col" class="first_th">', $txt['modSite_edit_id']  ,'</th>
-						<th scope="col">', $txt['modSite_edit_cat_name']  ,'</th>
-						<th scope="col" class="last_th">', $txt['modSite_edit_edit_delete'] ,'</th>
-					</tr>
-				</thead>
-			<tbody>';
-
-		foreach($context['modsite']['cats']['all'] as $all)
-		{
-			echo '
-				<tr class="windowbg" style="text-align: center">
-					<td>
-						', $all['id'] ,'
-					</td>
-					<td>
-						',$all['name'],'
-					</td>
-					<td>
-						', $context['modsite']['object']->crud($all['id'], 'cat') ,'
-					</td>
-				</tr>';
-		}
-
-		echo '
-			</tbody>
-		</table><br />';
-
-	/* A nice form for adding a new cat */
-	if ($context['modsite']['object']->permissions('add') == true)
-		echo '
-			<span class="clear upperframe">
-				<span></span>
-			</span>
-			<div class="roundframe rfix">
-				<div class="innerframe">
-					<form action="', $scripturl, '?action=modsite;sa=addCat" method="post" target="_self">
-						<dl id="post_header">
-							<dt>
-								<span id="caption_subject">', $txt['modSite_edit_create_cat']  ,'</span>
-							</dt>
-							<dd>
-								<input type="text" name="title" size="55" tabindex="1" maxlength="255" value="" class="input_text" /> <input type="submit" name="send" class="sbtn" value="', $txt['modSite_edit_create_cat']  ,'" />
-							</dd>
-						</dl>
-					</form>
-				</div>
-			</div>
-			<span class="clear lowerframe">
-				<span></span>
-			</span><br />';
+		<span class="clear lowerframe">
+			<span></span>
+		</span><br />';
 
 	echo '
 	</div>
