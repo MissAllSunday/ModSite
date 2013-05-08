d4899afd1da3ffc72b9226530311576ea6f52158
Suki <suki@missallsunday.com>
2013-05-07 20:05:47 -0500

    ModSiteParser
    Signed-off-by: Suki <suki@missallsunday.com>

---
+++ Sources/ModSite/ModSiteParser.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+ * @package ModSite mod
+ * @version 1.0
+ * @author Suki <missallsunday@simplemachines.org>
+ * @copyright 2013 Suki
+ */
+
+if (!defined('SMF'))
+	die('Hacking attempt...');
+
+
+class ModSiteParser
+{
+	public function github($username)
+	{
+		global $boarddir;
+
+		require_once ($boarddir .'/vendor/autoload.php');
+
+		$this->client = new Github\Client(
+			new Github\HttpClient\CachedHttpClient(array('cache_dir' => $boarddir .'/cache/github-api-cache'))
+		);
+		$this->username = $username;
+
+		return $this->client;
+	}
+
+	/**
+	 * Tries to fetch the content of a given url
+	 *
+	 * @access protected
+	 * @param string $url the url to call
+	 * @return mixed either the page requested or a boolean false
+	 */
+	protected function fetch_web_data($url)
+	{
+		global $sourcedir;
+
+		/* Safety first! */
+		if (empty($url))
+			return false;
+
+		/* I can haz cURL? */
+		if (function_exists ('curl_init'))
+		{
+			$ch = curl_init();
+
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_HEADER, false);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			$content = curl_exec($ch);
+			curl_close($ch);
+
+			/* Send the data directly, evil, I'm evil! :P */
+			return $content;
+		}
+
+		/* Good old SMF's fetch_web_data to the rescue! */
+		else
+		{
+			/* Requires a function in a source file far far away... */
+			require_once($sourcedir .'/Subs-Package.php');
+
+			/* Send the result directly, we are gonna handle it on every case */
+			return fetch_web_data($url);
+		}
+	}
+
+	protected function cleanCache($type)
+	{
+		if (empty($type))
+			return;
+
+		if (!is_array($type))
+			$type = array($type);
+
+		foreach ($type as $t)
+			cache_put_data(modsite::$name .'_'. $type, '', 120);
+	}
+}
